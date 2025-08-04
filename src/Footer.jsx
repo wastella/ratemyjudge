1c8e717f60fd7d9591d37f21f40f09d385314f30
wastella <william.a.stella@gmail.com>
2025-08-04 19:26:25 -0400

    footer fixed

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -9,14 +9,12 @@ const Footer = () => (
     padding: "1.5rem 0",
     marginTop: "3rem",
     fontSize: "0.95rem",
-    position: "fixed",
     left: 0,
     bottom: 0,
     width: "100%",
     zIndex: 100
   }}>
     <span>
-      © {new Date().getFullYear()} RateMyJudge &nbsp;|&nbsp; 
       <Link to="/terms" style={{ color: "#bbb", textDecoration: "underline" }}>Terms of Service</Link>
     </span>
   </footer>
